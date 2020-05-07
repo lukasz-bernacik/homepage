cb11d77c580f078e4fb9a7bee1ebfc9f5a380f4e
Łukasz Bernacik <lukasz.bernacik@gmail.com>
2020-05-07 07:41:00 +0200

    add subheading to about section and fill it with js and add opacity to navigation

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const age = 31;
 console.log ('Cześć, witaj na mojej stronie!');
 
 console.log (`Mam na imię ${name} i mam ${age} lat`);
+
+const subheading = document.querySelector('.about__subheading--js');
+
+console.log(subheading);
+
+subheading.innerHTML = `Mam na imię ${name} i mam ${age} lat`;
