58c9032935726a74374b5f5641e1c9c595e054e7
Łukasz Bernacik <lukasz.bernacik@gmail.com>
2020-05-17 06:59:55 +0200

    add images folder, completely rebuild html of header

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ console.log ('Cześć, witaj na mojej stronie!');
 
 console.log (`Mam na imię ${name} i mam ${age} lat`);
 
-const subheading = document.querySelector('.about__subheading--js');
+const subheading = document.querySelector('.welcome__subheading--js');
 
 console.log(subheading);
 
-subheading.innerHTML = `Mam na imię ${name} i mam ${age} lat`;
+subheading.innerHTML = `Hej! Mam na imię ${name} i mam ${age} lat`;
