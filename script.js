0c935d043e05b54c8f0b257366916c92a2976d40
Łukasz Bernacik <lukasz.bernacik@gmail.com>
2020-05-20 07:12:33 +0200

    add function to js

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ console.log ('Cześć, witaj na mojej stronie!');
 
 console.log (`Mam na imię ${name} i mam ${age} lat`);
 
-const subheading = document.querySelector('.welcome__subheading--js');
 
-console.log(subheading);
 
-subheading.innerHTML = `Hej! Mam na imię ${name} i mam ${age} lat`;
+function hello(age, firstName) {
+    console.log(`Hej mam na imię ${firstName} i mam ${age} lat`);
+};
+
+hello (31, 'Bercik');
+
+function createContent(querySelectorContent, content) {
+    const element = document.querySelector(querySelectorContent);
+    element.innerHTML = content;
+}
+
+createContent ('.welcome__subheading--js', `Hej! Mam na imię ${name} i mam ${age} lat`);
