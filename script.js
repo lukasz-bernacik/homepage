c34aa956fa9f31c277b4e5f71c58cb8d1745d77d
Łukasz Bernacik <lukasz.bernacik@gmail.com>
2020-06-03 06:47:50 +0200

    change structure of script.js file add hamburger menu

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
+const hamburger = document.querySelector('.navigation__hamburger--js');
+
+hamburger.addEventListener('click', () => {
+    const nav = document.querySelector('.menu--js')
+    nav.classList.toggle('menu--open');
+})
+
 const name = 'Łukasz';
 const age = 31;
-
-console.log ('Cześć, witaj na mojej stronie!');
-
-console.log (`Mam na imię ${name} i mam ${age} lat`);
-
-
-
-function hello(age, firstName) {
-    console.log(`Hej mam na imię ${firstName} i mam ${age} lat`);
-};
-
-hello (31, 'Bercik');
 
 function createContent(querySelectorContent, content) {
     const element = document.querySelector(querySelectorContent);
     element.innerHTML = content;
 }
 
-createContent ('.welcome__subheading--js', `Hej! Mam na imię ${name} i mam ${age} lat`);
+createContent ('.hero__subheading--js', `Hej! Mam na imię ${name} i mam ${age} lat`);
